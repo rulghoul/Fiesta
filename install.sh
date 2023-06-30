49b97fe7e18fbf3eb5567770ebbf1aac4047c89e
Raul Perez <ghoulrul@gmail.com>
2023-06-30 17:11:34 -0600

    Se actualizo el script de Eventos

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ export EVENTOS_SQL_PORT="3306"
 
 #Crear Usuario y asignar permisos
 groupadd --system webapps
-useradd --system --gid webapps --home /var/www/python/Fiesta eventos
+useradd --system --gid webapps --home "/var/www/python/Fiesta" eventos
 
 chown -R eventos:webapps /var/www/python/Fiesta
 chmod +x gunicorn_eventos.bash
@@ -37,19 +37,46 @@ mysql -h $DB_HOST -u $DB_USER -p$DB_PASSWORD -e "$SQL1" || exit 1
 #Crear entorno virtual y descargar las librerias
 python3 -m venv virtual
 source virtual/bin/activate
+
+# Exportar las variables de entorno al entorno virtual
+export EVENTOS_SQL_ENGINE
+export EVENTOS_SQL_DATABASE
+export EVENTOS_SQL_USER
+export EVENTOS_SQL_PASSWORD
+export EVENTOS_SQL_HOST
+export EVENTOS_SQL_PORT
+
+echo "Instalar librerias"
 pip3 install -r requirements.txt
 cd fiestas
+
+echo "Recreando tablas..."
+
 python3 manage.py migrate
+
+echo "Generando archivos estaticos..."
+
 python3 manage.py collectstatic
 python3 manage.py createsuperuser
-python3 mage.py loaddata inicio.json
-python3 mage.py loaddata catalogos02.json
+
+echo "Repoblando tablas..."
+
+python3 manage.py loaddata inicio.json
+python3 manage.py loaddata catalogos02.json
+
+echo "Generando usuario principal"
+
+python3 manage.py createsuperuser
 cd ..
 deactivate
+
+echo "Se termino de configurar el sistema con python"
 mkdir logs
 mkdir run
 
 #Instalar supervisor
+
+echo "Se crea el servicio Eventos con supervisor..."
 
 cp eventos.conf /etc/supervisor/conf.d/eventos.conf
 #Actualizar supervisor
@@ -57,7 +84,9 @@ supervisorctl reread
 supervisorctl update
 supervisorctl status
 
+echo "Se termino de crear elservicio eventos"
 
+echo "Se copia la configuracion de eventos"
 #Configuracion de Nginx
 cp nginx /etc/nginx/sites-available/eventos
 #Activar sitio Nginx
